8bc48f9b5557c9356ae0928474dfda36088860ab
Temidola <temiandlovelies@gmail.com>
2023-05-10 08:57:12 +0100

    Latest commit

--- src/MedApp/DashboardDoctor.js
+++ src/MedApp/DashboardDoctor.js
@@ -12,20 +12,42 @@ const DashboardDoctor = () => {
   const [BloodGroup, setBloodGroup] = useState("");
   const [DateBirth, setDateBirth] = useState("");
  const [PatientDetails, setPatientDetails] = useState([]);
+ const [idVariable, setidVariable] = useState("")
 
   const Create = () => { 
 
-    axios.post("http://localhost:2000/user/patientDetails",{PatientName,KinName,DateBirth}).then((response) => { 
+    axios.post("http://localhost:2000/user/PatientDetails",{PatientName,KinName,Genotype,BloodGroup,DateBirth}).then((response) => { 
       console.log(response)
     }).catch((err) => { 
       console.log(err);
     })
 
-    console.log(PatientName,KinName,Genotype,BloodGroup,DateBirth);
+   
   }
 
+
+
+  const SendUpdateDetails = () => { 
+    axios.post("http://localhost:2000/user/updatePatientDetails",{PatientName,KinName,Genotype,BloodGroup,DateBirth}).then((response)=> { 
+      console.log(response)
+    }).catch((err) => {
+      console.log(err);
+    })
+
+   
+    PatientDetails.map((eachItem) => { 
+      axios.post("http://localhost:2000/user/updatePatientDetails",eachItem._id).then((response) => { 
+        console.log(response)
+      }).catch((err) => { 
+        console.log(err);
+      })
+    })
+
+  }
+
+  
   const getPatientDetails = () => { 
-    axios.get("http://localhost:2000/user/patientDetails").then((response) => { 
+    axios.get("http://localhost:2000/user/updatePatientDetails").then((response) => { 
     
       setPatientDetails(response.data)
       
@@ -43,6 +65,124 @@ const DashboardDoctor = () => {
     // })
   return (
     <div> 
+
+      {PatientDetails.map((eachDetails,index) => { 
+        return ( 
+          <> 
+            {/* <!-- Button trigger modal --> */}
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+                  Update Patient Details
+                </button>
+
+                {/* <!-- Modal --> */}
+                <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                  <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h5 class="modal-title text-danger " id="exampleModalLongTitle">Edit Patient Details here</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                        </button>
+                      </div>
+                      {/* Modal Body */}
+                      <div class="modal-body ink">
+                      <div className="form-group mt-3"> 
+             <label htmlFor="">Name:</label>
+             <input onChange={(e) => setPatientName(e.target.value)}  type="text" className="form-control ink" placeholder='Patient Name' />
+
+             </div>
+
+             <div className="form-group mt-3"> 
+             <label htmlFor="">Next of Kin:</label>
+              <input onChange={(e) => setKinName(e.target.value)} type="text" className="form-control ink " placeholder='Name of Next of Kin' />
+            
+             
+             </div>
+
+             <div className="form-group mt-3"> 
+             <label htmlFor="">Genotype:</label>
+             <select onChange={(e) => setGenotype(e.target.value)}   className='form-select form-control ink ' name="" id="">  
+                <option selected > Your Genotype </option>
+                <option value="AA">AA</option>
+                <option value="AS">AS</option>
+                <option value="SS">SS</option>
+                <option value="TT">TT</option>
+              </select>
+             
+             </div>
+
+             <div className="form-group mt-3"> 
+             <label htmlFor="">Blood group:</label>
+                <select onChange={(e) => setBloodGroup(e.target.value)} className='form-select form-control ink ' name="" id="">  
+                    <option selected > Blood group </option>
+                    <option value="-A">-A</option>
+                    <option value="B">B</option>
+                    <option value="AB">AB</option>
+                    <option value="O">O</option>
+                    
+                  </select>
+               
+             
+             </div>
+
+             <div className="form-group mt-3"> 
+             <label htmlFor="">Date of birth:</label>
+             <input onChange={(e) => setDateBirth(e.target.value)}  type="date" className="form-control   ink" />
+             
+             </div>
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button onClick={SendUpdateDetails} type="button" class="btn btn-primary">Save changes</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+             
+
+                {/* <h1> Patient Name:{eachDetails.PatientName}</h1>
+                <h1> Next of Kin:{eachDetails.KinName}</h1>
+                <h1> Date of Birth:{eachDetails.DateBirth}</h1> */}
+
+
+                            
+<table class="table table-bordered mt-5">
+  <thead>
+    <tr>
+      <th scope="col">S/N</th>
+      <th scope="col">Patient Name</th>
+      <th scope="col">Next of Kin</th>
+      <th scope="col">Date of birth</th>
+      <th scope="col">Update Patient Information</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td> {eachDetails.PatientName} </td>
+      <td> {eachDetails.KinName} </td>
+      <td>{eachDetails.DateBirth}</td>
+      <td>{eachDetails._id}</td>
+      <td><button className='btnIn'>Update Information</button></td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+
+
+          </>
+        )
+      })}
         <section className='allWrap'> 
               <h1 className='text-center text-white'> Doctors report.</h1>
 
@@ -63,10 +203,10 @@ const DashboardDoctor = () => {
              <label htmlFor="">Genotype:</label>
              <select onChange={(e) => setGenotype(e.target.value)}   className='form-select form-control ink ' name="" id="">  
                 <option selected > Your Genotype </option>
-                <option value="1">AA</option>
-                <option value="2">AS</option>
-                <option value="3">SS</option>
-                <option value="4">TT</option>
+                <option value="AA">AA</option>
+                <option value="AS">AS</option>
+                <option value="SS">SS</option>
+                <option value="TT">TT</option>
               </select>
              
              </div>
@@ -75,10 +215,10 @@ const DashboardDoctor = () => {
              <label htmlFor="">Blood group:</label>
                 <select onChange={(e) => setBloodGroup(e.target.value)} className='form-select form-control ink ' name="" id="">  
                     <option selected > Blood group </option>
-                    <option value="1">-A</option>
-                    <option value="2">B</option>
-                    <option value="3">AB</option>
-                    <option value="4">O</option>
+                    <option value="-A">-A</option>
+                    <option value="B">B</option>
+                    <option value="AB">AB</option>
+                    <option value="O">O</option>
                     
                   </select>
                
@@ -94,17 +234,11 @@ const DashboardDoctor = () => {
              <button  onClick={Create} className="btnIn">Create report</button>
              <button className='btnIn mt-5' onClick={getPatientDetails}> View Patient results here  </button>
 
-             <h3 className='text-danger'> {PatientDetails.map((eachDetails) => ( 
-              <> 
-                <h1> Patient Name:{eachDetails.PatientName}</h1>
-                <h1> Next of Kin:{eachDetails.KinName}</h1>
-                <h1> Date of Birth:{eachDetails.DateBirth}</h1>
-              
-              </>
-             ))} </h3>
 
-             
 
+           
+
+ 
 
         </section>
        
